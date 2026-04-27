b9af1401b22e928ab9625abafab2a94d79954530
yemathekaramelo-hash <yemathekaramelo@gmail.com>
2026-04-28 05:00:21 +0800

    B: added screen and game loop

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import pygame
+from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    ## sets screen size
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+    screen.fill("black")
+    pygame.display.flip()
+                
+
+  
+
+
 
 
 if __name__ == "__main__":
